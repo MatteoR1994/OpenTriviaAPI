a1fd4ca3a4e211dee736a48a22ac26066e8afd72
ferrucogo <43414055+ferrucogo@users.noreply.github.com>
2022-02-24 12:11:40 +0100

    Removed pop-up

--- script.js
+++ script.js
@@ -213,8 +213,8 @@ var downloadTimer = setInterval(function(){
 
 var userLang = document.getElementsByTagName("html")[0].getAttribute("lang");
 if(userLang == "en"){
-    if (confirm('This website is for destinated to italian users. Do you want to get a live translated version?')) {
-        window.open('https://ferrucogo-github-io.translate.goog/OpenTriviaAPI/?_x_tr_sl=en&_x_tr_tl=it&_x_tr_hl=it&_x_tr_pto=wapp');
+    if (confirm('This website is addressed to italian users. Do you want to get a live translated version?')) {
+        $('a').on('click.open', function(e) { e.preventDefault(); window.open('https://ferrucogo-github-io.translate.goog/OpenTriviaAPI/?_x_tr_sl=en&_x_tr_tl=it&_x_tr_hl=it&_x_tr_pto=wapp')});
       } else {
         // Do nothing!
         console.log('Nothing.');
